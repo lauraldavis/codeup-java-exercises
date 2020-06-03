b59f8fd62cbaaf015f24806d3019cc5468269b32
Laura Davis <laura.l.davis.210@gmail.com>
2020-06-03 13:41:48 -0500

    Java I - Control Statements/Loops - exercise #3 Table of powers

--- ControlFlowExercises.java
+++ ControlFlowExercises.java
@@ -1,3 +1,6 @@
+import java.util.Scanner;
+import java.util.Formatter;
+
 public class ControlFlowExercises {
     public static void main(String[] args){
         // After each exercise, commit changes & replace code with the next exercise
@@ -63,21 +66,38 @@ public class ControlFlowExercises {
 //            System.out.printf("%d%n", i);
 //        }
 
-        // 2. Fizzbuzz
-        // Print numbers 1 to 100, except for multiple of 3 print "Fizz" / multiple of 5 print "Buzz" / multiple of 3 and 5 print "FizzBuzz"
-        for (int i = 1; i <= 100; i++) {
-            if (i % 5 == 0 && i % 3 == 0) {
-                System.out.println("FizzBuzz");
-            } else if (i % 3 == 0) {
-                System.out.println("Fizz");
-            } else if (i % 5 == 0) {
-                System.out.println("Buzz");
-            } else {
-                System.out.println(i);
+//        // 2. Fizzbuzz
+//        // Print numbers 1 to 100, except for multiple of 3 print "Fizz" / multiple of 5 print "Buzz" / multiple of 3 and 5 print "FizzBuzz"
+//        for (int i = 1; i <= 100; i++) {
+//            if (i % 5 == 0 && i % 3 == 0) {
+//                System.out.println("FizzBuzz");
+//            } else if (i % 3 == 0) {
+//                System.out.println("Fizz");
+//            } else if (i % 5 == 0) {
+//                System.out.println("Buzz");
+//            } else {
+//                System.out.println(i);
+//            }
+//        }
+
+        // 3. Display a table of powers
+        // Prompt user to enter an integer. Display a table of squares and cubes from 1 to the value entered.
+        // ask if the user wants to continue. Assume user enters valid data. Only continue if user agrees to.
+        Scanner sc = new Scanner(System.in);
+        String proceed;
+        do {
+            System.out.print("Please enter an integer: ");
+            int userInt = sc.nextInt();
+            System.out.printf("You entered: %d%n", userInt);
+            System.out.printf("Here is your table: %n%n");
+            System.out.printf("number | squared | cubed%n");
+            System.out.printf("------ | ------- | -----%n");
+            for (int i = 1; i <= userInt; i++) {
+                System.out.printf("%-6d | %-7.0f | %-5.0f %n", i, Math.pow(i, 2), Math.pow(i, 3));
             }
-        }
-
-
+            System.out.print("Would you like to continue? (y/n): ");
+            proceed = sc.next();
+        } while ( proceed.equalsIgnoreCase("y") );
 
 
 
