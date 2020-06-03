03c2effa0598875919bbccd3b1693bbb54809406
Laura Davis <laura.l.davis.210@gmail.com>
2020-06-03 10:40:55 -0500

    Java I - Control Statements/Loops - exercise #1b, 2nd part of do-while

--- ControlFlowExercises.java
+++ ControlFlowExercises.java
@@ -14,18 +14,20 @@ public class ControlFlowExercises {
 //        }
 
         // 1b. Do While
-        // Create do-while loop that counts by 2's starting w/0 & ending at 100. Follow each w/new line.
 
-        int i = 0;
+//        // Create do-while loop that counts by 2's starting w/0 & ending at 100. Follow each w/new line.
+//        int i = 0;
+//        do {
+//            System.out.printf("%d%n", i);
+//            i += 2;
+//        } while (i <= 100);
+
+        // Alter loop to count backwards by 5's from 100 to -10
+        int i = 100;
         do {
             System.out.printf("%d%n", i);
-            i += 2;
-        } while (i <= 100);
-
-
-
-
-
+            i -= 5;
+        } while (i >= -10);
 
 
 
