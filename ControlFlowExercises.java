865039d011b4ea1f0895e846fc169d51fc4f5e41
Laura Davis <laura.l.davis.210@gmail.com>
2020-06-03 15:10:46 -0500

    Java I - Control Statements/Loops - exercise #4 Number to letter grades

--- ControlFlowExercises.java
+++ ControlFlowExercises.java
@@ -80,27 +80,63 @@ public class ControlFlowExercises {
 //            }
 //        }
 
-        // 3. Display a table of powers
-        // Prompt user to enter an integer. Display a table of squares and cubes from 1 to the value entered.
-        // ask if the user wants to continue. Assume user enters valid data. Only continue if user agrees to.
+//        // 3. Display a table of powers
+//        // Prompt user to enter an integer. Display a table of squares and cubes from 1 to the value entered.
+//        // ask if the user wants to continue. Assume user enters valid data. Only continue if user agrees to.
         Scanner sc = new Scanner(System.in);
+//        String proceed;
+//        do {
+//            System.out.print("Please enter an integer: ");
+//            int userInt = sc.nextInt();
+//            System.out.printf("You entered: %d%n", userInt);
+//            System.out.printf("Here is your table: %n%n");
+//            System.out.printf("number | squared | cubed%n");
+//            System.out.printf("------ | ------- | -----%n");
+//            for (int i = 1; i <= userInt; i++) {
+//                System.out.printf("%-6d | %-7.0f | %-5.0f %n", i, Math.pow(i, 2), Math.pow(i, 3));
+//            }
+//            System.out.print("Would you like to continue? (y/n): ");
+//            proceed = sc.next();
+//        } while ( proceed.equalsIgnoreCase("y") );
+
+        // 4. Convert number grades to letter grades
+        // Prompt user for number grade from 0 to 100. Display letter grade, prompt user to continue. Assume valid integers for grades.
+        // Only continue if user agrees to. A: 100-88, B: 87-80, C:79-67, D: 66-60, F: 59-0
+        // Bonus: edit grade ranges to include pluses and minuses, ex: 99-100 = A+. (no standards available, just slice off top/bottom 2 pts...)
         String proceed;
         do {
-            System.out.print("Please enter an integer: ");
+            System.out.print("Please enter a numerical grade from 0 to 100: ");
             int userInt = sc.nextInt();
-            System.out.printf("You entered: %d%n", userInt);
-            System.out.printf("Here is your table: %n%n");
-            System.out.printf("number | squared | cubed%n");
-            System.out.printf("------ | ------- | -----%n");
-            for (int i = 1; i <= userInt; i++) {
-                System.out.printf("%-6d | %-7.0f | %-5.0f %n", i, Math.pow(i, 2), Math.pow(i, 3));
+            if (userInt >= 99 && userInt <= 100) { // 88-100 A
+                System.out.println("A+");
+            } else if (userInt >= 90 && userInt <= 98) {
+                System.out.println("A");
+            } else if (userInt >= 88 && userInt <= 89) {
+                System.out.println("A-");
+            } else if (userInt >= 86 && userInt <= 87) { // 80-87 B
+                System.out.println("B+");
+            } else if (userInt >= 82 && userInt <= 85) {
+                System.out.println("B");
+            } else if (userInt >= 80 && userInt <= 81) {
+                System.out.println("B-");
+            } else if (userInt >= 78 && userInt <= 79) { // 67-79 C
+                System.out.println("C+");
+            } else if (userInt >= 69 && userInt <= 77) {
+                System.out.println("C");
+            } else if (userInt >= 67 && userInt <= 68) {
+                System.out.println("C-");
+            } else if (userInt >= 65 && userInt <= 66) { // 60-66 D
+                System.out.println("D+");
+            } else if (userInt >= 62 && userInt <= 64) {
+                System.out.println("D");
+            } else if (userInt >= 60 && userInt <= 61) {
+                System.out.println("D-");
+            } else if (userInt >= 0 && userInt <= 59) {
+                System.out.println("F");
             }
             System.out.print("Would you like to continue? (y/n): ");
             proceed = sc.next();
         } while ( proceed.equalsIgnoreCase("y") );
-
-
-
 
 
     }
