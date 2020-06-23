de7e4f62326985fc2a6e555c14bdf55f305b04b5
Laura Davis <laura.l.davis.210@gmail.com>
2020-06-22 21:21:10 -0500

    Java II - Collections - work on bonuses

--- grades/GradesApplication.java
+++ grades/GradesApplication.java
@@ -2,6 +2,7 @@ package grades;
 
 import java.util.HashMap;
 import java.util.Map;
+
 import util.Input;
 
 public class GradesApplication {
@@ -38,7 +39,7 @@ public class GradesApplication {
         s5.addGrade(90);
         s5.addGrade(95);
         System.out.println(s5.getName() + " " + s5.getGrades());
-        students.put("lauraldavis",s1);
+        students.put("lauraldavis", s1);
         students.put("MarkTAlonge", s2);
         students.put("AndrewBrought", s3);
         students.put("juliacontreras483", s4);
@@ -47,30 +48,75 @@ public class GradesApplication {
 //        System.out.println(students);
         // {juliacontreras483=grades.Student@2c8d66b2, lauraldavis=grades.Student@5a39699c, AndrewBrought=grades.Student@3cb5cdba, MarkTAlonge=grades.Student@56cbfb61, cottoknoah=grades.Student@1134affc}
 
-        boolean yesNo;
+        boolean yesNo = false;
         do {
-            String gitHubUsers = "* ";
+            String intPrompt = "What would you like to do?\n" +
+                    "0 - exit\n" +
+                    "1 - view all grades for all students.\n" +
+                    "2 - view the overall class average grade.\n" +
+                    "3 - select a student to view more information about.\n" +
+                    "Enter your choice: ";
+            int numIntRangePrompt = in.getInt(0, 3, intPrompt); // edit when menu expands
+            System.out.println("You entered: " + numIntRangePrompt);
 
-            for (String hashname : students.keySet())
-            gitHubUsers += hashname + " * ";
+            switch (numIntRangePrompt) {
+                case 0:
+                    System.out.println("Bye!");
+                    yesNo = false;
+                    break;
+                case 1:
+                    String textAllGrades = "";
+                    for (String hashname : students.keySet()) {
+                        textAllGrades = hashname + " - Grades: " + students.get(hashname).getGrades();
+                        textAllGrades += " Current Average: " + students.get(hashname).getGradeAverage();
+                        System.out.println(textAllGrades);
+                    }
+                    yesNo = true;
+                    break;
+                case 2:
+                    double totalAvgGrades = 0;
+                    for (String hashname : students.keySet()) {
+                        totalAvgGrades += students.get(hashname).getGradeAverage();
+                    }
+                        System.out.println("Overall Average Grade: " + totalAvgGrades/students.size());
+                    yesNo = true;
+                    break;
+                case 3:
+                    // nested do/while loop
+                    do {
+                        String gitHubUsers = "* ";
+                        String textUserGrades;
 
-            System.out.println("Welcome!\n\nHere are the GitHub usernames of our students:\n" + gitHubUsers);
+                        for (String hashname : students.keySet())
+                            gitHubUsers += hashname + " * ";
 
-            String strPrompt = "What student would you like to see more information on?\n";
-            String userInput = in.getString(strPrompt);
+                        System.out.println("Welcome!\n\nHere are the GitHub usernames of our students:\n" + gitHubUsers);
 
-            if (students.containsKey(userInput)) {
-                System.out.println("Name: " + students.get(userInput).getName() + " - GitHub Username: " + userInput + "\nCurrent Average: " + students.get(userInput).getGradeAverage() + "\n\n");
-                String boolPrompt = "Would you like to see another student? (y/n)";
-                yesNo = in.yesNo(boolPrompt);
-            } else {
-                System.out.println("You entered: " + userInput + "\n");
-                System.out.println("That GitHub username is not in our student list.\n");
-                String boolPrompt = "Do you want to continue? (y/n): ";
-                yesNo = in.yesNo(boolPrompt);
+                        String strPrompt = "What student would you like to see more information on?\n";
+                        String userInput = in.getString(strPrompt);
+
+                        if (students.containsKey(userInput)) {
+                            textUserGrades = "Name: " + students.get(userInput).getName() + " - GitHub Username: " + userInput;
+                            textUserGrades += "\nGrades: " + students.get(userInput).getGrades();
+                            textUserGrades += "\nCurrent Average: " + students.get(userInput).getGradeAverage() + "\n\n";
+                            System.out.println(textUserGrades);
+                            String boolPrompt = "Would you like to see another student? (y/n)";
+                            yesNo = in.yesNo(boolPrompt);
+                        } else {
+                            System.out.println("You entered: " + userInput + "\n");
+                            System.out.println("That GitHub username is not in our student list.\n");
+                            String boolPrompt = "Do you want to continue? (y/n): ";
+                            yesNo = in.yesNo(boolPrompt);
+                        }
+                    } while (yesNo); // end nested do/while loop
+                    yesNo = true;
+                    break;
+                default:
+                    System.out.println("Please enter one of the options.");
+                    yesNo = true;
+
             }
-        }while(yesNo);
-
+        } while (yesNo);
 //        // Iterating over Map.entrySet() using For-Each loop (most common, use if you need both keys and values in the loop)
 //        for (Map.Entry<String, Student> entry : students.entrySet())
 //            System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
